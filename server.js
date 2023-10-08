3626dcfca890e03344af51a8da484100b6890c3d
mbiskho <khomeini.bisma@gmail.com>
2023-10-08 20:32:33 +0700

    fix : cors

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ class Server {
   }
 
   starter() {
-    this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(cors())
-    this.app.options('*', cors());
+    this.app.use(cors({
+      origin: '*', // use your actual domain name (or localhost), using * is not recommended
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+      credentials: true
+    }))
+    this.app.use(express.json());
     this.app.use(apiErrorHandler);
     this.app.use('/', router);
   }
